7cf716c683a61161a74780709cffa68c65215a37
RenanDeveloper <renantameirao@gmail.com>
2022-04-29 17:59:39 -0300

    inclusão do arquivo criptografia.js e declaração das funções

---
+++ criptografia.js
@@ -0,0 +1,9 @@
+function codificar(){
+    var textoAlvo = document.querySelector('#texto').value;
+    console.log(textoAlvo);
+}
+
+function decodificar(){
+    var textoAlvo = document.querySelector('#texto').value;
+    console.log(textoAlvo);
+}
