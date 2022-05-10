f3150f7254efb72910448a6b7bef24818c2b40cf
RenanDeveloper <renantameirao@gmail.com>
2022-05-10 09:30:00 -0300

    inclusão pasta img, inclusão da imagem e logica para sumir, remoção de console.log, centralização dos botões

--- criptografia.js
+++ criptografia.js
@@ -1,11 +1,21 @@
+function aguardando(){
+    var saida = document.querySelector('.resultado');
+
+    saida.textContent="";
+    setInterval(saida.style.backgroundImage = "url('img/boneco.png')", 1000);
+}
+
 function codificar(){
-    var textoAlvo = document.querySelector('#texto').value;
-    console.log("texto inicial: " + textoAlvo);
+    var textoAlvo = document.querySelector('.texto').value;
+    var saida = document.querySelector('.resultado');
+
+    saida.style.backgroundImage = "none";
+
+
     textoAlvo = Array.from(textoAlvo);
 
     for (let i = 0; i < textoAlvo.length; i++) {
         const letra = textoAlvo[i];
-        console.log("letra atual: " + letra)
         if (letra == 'a') {
             textoAlvo.splice(i, 1, "ai");
         }
@@ -24,53 +34,50 @@ function codificar(){
     }
 
     textoAlvo = textoAlvo.join("");
-    console.log("texto posterior: " + textoAlvo);
-    var saida = document.querySelector('#resultado');
     saida.textContent = textoAlvo;
 
 
 }
+
 function decodificar(){
-    var textoAlvo = document.querySelector('#texto').value;
-    console.log("texto inicial: " + textoAlvo);
+    var textoAlvo = document.querySelector('.texto').value;
+    var saida = document.querySelector('.resultado');
+
+    saida.style.backgroundImage = "none";
+
     
     textoAlvo = textoAlvo.split("ai");
     for (let i = 0; i < textoAlvo.length-1; i++) {
         const letra = textoAlvo[i];
-        console.log("letra atual: " + letra);
         textoAlvo[i] = textoAlvo[i]+'a';
     }
     textoAlvo = textoAlvo.join(""); 
     textoAlvo = textoAlvo.split("enter");
     for (let i = 0; i < textoAlvo.length-1; i++) {
         const letra = textoAlvo[i];
-        console.log("letra atual: " + letra);
         textoAlvo[i] = textoAlvo[i]+'e';
     }
     textoAlvo = textoAlvo.join(""); 
     textoAlvo = textoAlvo.split("imes");
     for (let i = 0; i < textoAlvo.length-1; i++) {
         const letra = textoAlvo[i];
-        console.log("letra atual: " + letra);
         textoAlvo[i] = textoAlvo[i]+'i';
     }
     textoAlvo = textoAlvo.join(""); 
     textoAlvo = textoAlvo.split("ober");
     for (let i = 0; i < textoAlvo.length-1; i++) {
         const letra = textoAlvo[i];
-        console.log("letra atual: " + letra);
         textoAlvo[i] = textoAlvo[i]+'o';
     }
     textoAlvo = textoAlvo.join(""); 
     textoAlvo = textoAlvo.split("ufat");
     for (let i = 0; i < textoAlvo.length-1; i++) {
         const letra = textoAlvo[i];
-        console.log("letra atual: " + letra);
         textoAlvo[i] = textoAlvo[i]+'u';
     }
 
     textoAlvo = textoAlvo.join(""); 
-    console.log("texto posterior: " + textoAlvo);
-    var saida = document.querySelector('#resultado');
+    var saida = document.querySelector('.resultado');
     saida.textContent = textoAlvo;
 }
+
